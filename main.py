2943c40973110364e5f49c8a03b84053a51b8ffb
AlexMarchi2000 <alex.marchi2000@gmail.com>
2025-09-01 09:26:02 +0200

    add player forward and backward movement

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ def main():
 
         screen.fill("black")
 
+        player.update(dt)
         player.draw(screen)
 
         # updates entire screen
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        player.update(dt)
 
 
 if __name__ == "__main__":
