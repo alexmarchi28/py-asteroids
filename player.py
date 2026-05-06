eb12917ab78017cc54689cc2dc6d768040092ce2
alexmarchi28 <alex.marchi2000@gmail.com>
2026-05-06 20:50:47 +0200

    add inertia, add acceleration and brake

--- player.py
+++ player.py
@@ -24,9 +24,25 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
-    def move(self, dt):
+    def accelerate(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt
+        self.velocity += forward * PLAYER_ACCELERATION * dt
+        if self.velocity.length() > PLAYER_MAX_SPEED:
+            self.velocity.scale_to_length(PLAYER_MAX_SPEED)
+
+    def brake(self, dt):
+        speed = self.velocity.length()
+        if speed == 0:
+            return
+
+        new_speed = speed - PLAYER_BRAKE_DECELERATION * dt
+        if new_speed <= 0:
+            self.velocity.update(0, 0)
+        else:
+            self.velocity.scale_to_length(new_speed)
+
+    def move(self, dt):
+        self.position += self.velocity * dt
 
     def shoot(self):
         shot = Shot(self.position.x, self.position.y)
@@ -37,16 +53,17 @@ class Player(CircleShape):
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a]:
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.rotate(-dt)
-        if keys[pygame.K_d]:
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.rotate(dt)
-        if keys[pygame.K_w]:
-            self.move(dt)
-        if keys[pygame.K_s]:
-            self.move(-dt)
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.accelerate(dt)
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.brake(dt)
         if keys[pygame.K_SPACE]:
             if self.cooldown <= 0:
                 self.shoot()
 
+        self.move(dt)
         self.cooldown -= dt
