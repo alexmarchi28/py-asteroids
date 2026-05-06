9ee8ff0b5841400f2b40e7ecdc3b980b4b586107
alexmarchi28 <alex.marchi2000@gmail.com>
2026-05-06 20:43:49 +0200

    now start fullscreen, 1920x1080 scaled, hidden cursor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -10,7 +9,11 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode(
+        (SCREEN_WIDTH, SCREEN_HEIGHT),
+        pygame.FULLSCREEN | pygame.SCALED,
+    )
+    pygame.mouse.set_visible(False)
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
@@ -25,34 +28,40 @@ def main():
     asteroid = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
+    try:
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    return
 
-        screen.fill("black")
+            screen.fill("black")
 
 
-        for thing in updatable:
-            thing.update(dt)
+            for thing in updatable:
+                thing.update(dt)
 
-        for asteroid in asteroids:
+            for asteroid in asteroids:
 
-            if asteroid.collides(player):
-                print("Game Over!")
-                sys.exit(0)
+                if asteroid.collides(player):
+                    print("Game Over!")
+                    return
 
-            for shot in shots:
-                if asteroid.collides(shot):
-                    asteroid.split()
-                    shot.kill()
+                for shot in shots:
+                    if asteroid.collides(shot):
+                        asteroid.split()
+                        shot.kill()
 
-        for thing in drawable:
-            thing.draw(screen)
+            for thing in drawable:
+                thing.draw(screen)
 
-        # updates entire screen
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
+            # updates entire screen
+            pygame.display.flip()
+            dt = clock.tick(60) / 1000
+    finally:
+        pygame.mouse.set_visible(True)
+        pygame.quit()
 
 
 if __name__ == "__main__":
